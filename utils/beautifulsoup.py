d6736ebb595c4ada91fcee06f525dbd367b0322f
Zase55 <sergiocobo90@hotmail.es>
2025-01-21 16:40:10 +0100

    refactor: move logic to scrping_cards.py

--- utils/beautifulsoup.py
+++ utils/beautifulsoup.py
@@ -2,6 +2,7 @@ from bs4 import BeautifulSoup
 from playwright.sync_api import Page
 from typing import Callable
 from bs4.element import Tag
+from utils.scraping_cards import get_elements_dict, get_jobs_dict, get_categories_dict, get_abilities_dict, get_property_value
 
 
 def get_html_content(page: Page, url: str, timeout: int = 10000, parser: str = "html.parser") -> BeautifulSoup:
@@ -21,11 +22,6 @@ def get_image_detail(soup: BeautifulSoup, specific_atr: str|None, dynamic_filter
             return image[specific_atr]
         else:
             return image
-        
-def get_property_value(span: Tag):
-    property_value: str = span.text
-    property_value = property_value.replace("\n", "")
-    return property_value.strip()
 
 def get_title_card_detail(soup: BeautifulSoup) -> dict:
     properties = {}
@@ -35,6 +31,8 @@ def get_title_card_detail(soup: BeautifulSoup) -> dict:
     properties["Number"] = div.span.h3.text
     properties["Image"] = get_image_detail(soup, "src", lambda image: "https" in image["src"])
     
+    properties["Elements"] = []
+    properties["Jobs"] = []
     properties["Categories"] = []
     properties["Abilities"] = []
               
@@ -43,43 +41,14 @@ def get_title_card_detail(soup: BeautifulSoup) -> dict:
     trs = all_trs[:11]
     
     for i,tr in enumerate(trs):
-        if i == 8:
-            categories_tr = all_trs[i]
-            tds = categories_tr.find_all("td")
-            categories_span = tds[1].span.span.find_all("span")
-            for category in categories_span:
-                properties["Categories"].append(get_property_value(category))
+        if i == 2:
+            properties = get_elements_dict(all_trs, i, properties)
+        elif i == 7:
+            properties = get_jobs_dict(all_trs, i, properties)
+        elif i == 8:
+            properties = get_categories_dict(all_trs, i, properties)
         elif i == 10:
-            abilities_tr = all_trs[i]
-            abilities_divs = abilities_tr.find_all("span")[1].span.find_all("div", recursive=False)
-            for abilities_div in abilities_divs:
-                n_abilities = []
-                for item in abilities_div.contents:
-                    item_abilities = {}
-                    if item == "\n":
-                        continue
-                    if not isinstance(item, Tag):
-                        item_abilities["text"] = str(item).strip()
-                        n_abilities.append(item_abilities)
-                        continue
-                        
-                    if "src" in item.attrs:
-                        if "assets" in item["src"]:
-                            item_abilities["img"] = f"https://ffdecks.com/{item['src']}"
-                    else:
-                        item_value = get_property_value(item)
-                        if item.name == 'b':
-                            item_abilities["b"] = item_value
-                        
-                        elif item.name == 'i':
-                            item_abilities["i"] = item_value
-                        
-                        elif item.find("strong"):
-                            item_abilities["strong"] = item_value
-
-                    n_abilities.append(item_abilities)
-                
-                properties["Abilities"].append(n_abilities)
+            properties = get_abilities_dict(all_trs, i, properties)
         else:
             tds = tr.find_all("td")
             properties[tds[0].span.text.strip()] = get_property_value(tds[1].span)
